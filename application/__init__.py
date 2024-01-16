0a75c440145ca4e551b1b68b36a318a52e83e421
Billy Ndegwah Micah <80063605+Billy-06@users.noreply.github.com>
2024-01-16 09:43:16 +0000

    Added Chat Function

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,7 @@
 from application.config import DevConfig, ProdConfig
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -8,5 +9,6 @@ app.templates_folder='templates'
 
 app.config.from_object(DevConfig)
 
+socketio = SocketIO(app)
 db = SQLAlchemy(app)
 
